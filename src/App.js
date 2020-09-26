0076c8effe4efb689c748322f679db1dad791d5b
wassimnassour <nassourwassim@gmail.com>
2020-09-26 14:48:17 +0100

    add redirect if user is signin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { auth } from "./firebase/firebase";
 import "./App.css";
 
-function App() {
-  return <></>;
+function App({ history }) {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		auth.onAuthStateChanged(async (_usr) => {
+			if (_usr) {
+				history.push("/dashboard");
+			} else {
+				history.push("/signin");
+			}
+		});
+	}, [history]);
+	return <></>;
 }
 
 export default App;
