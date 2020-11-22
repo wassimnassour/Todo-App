d23c51a8a380cc886261ebda79f82ab2509f7953
was <nassourwassim@gmail.com>
2020-11-22 18:47:32 +0100

    add check if user exist or not

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import { auth } from "./firebase/firebase";
 
-function App() {
-  return <></>;
+function App({ history }) {
+	useEffect(() => {
+		auth.onAuthStateChanged(async (_usr) => {
+			if (_usr) {
+				history.push("/dashboard");
+			} else {
+				history.push("/signin");
+			}
+		});
+	}, [history]);
+	return <></>;
 }
 
 export default App;
